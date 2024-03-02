8fc4b71eef4b9b93cbaea049f9b22a969b8e41c7
dheeralbhole <dheeralbhole@gmail.com>
2024-03-02 16:19:30 -0600

    Updated setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Dheeral",
+    author_email="dheeralbhole@gmail.com",
+    packages=find_packages()
+)
